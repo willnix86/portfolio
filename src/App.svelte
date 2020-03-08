f0fd6d9f1ad63a133c29833a018bf162d61af358
Will Nixon <will_nixon@me.com>
2020-03-08 09:12:17 -0400

    initialised now

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<style>
+  main {
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 60px 0 0 0;
+    text-align: center;
+  }
+</style>
+
+<main>
+  <Portfolio />
+</main>
---
+++ src/strings.js
@@ -0,0 +1,51 @@
+export const hero = {
+  title: "LEMMI",
+  subtitle: "The app that lets you chat!",
+  description: "Lemmi helps people who have difficulty with speech communicate with ease, and re-connect with others.",
+};
+
+export const features = [
+  {
+    title: "Mobile",
+    description: "Lemmi is available on iOS and Android devices and is accessible anywhere. Once downloaded, it works with or without a network connection."
+  },
+  {
+    title: "Simple",
+    description: "The app's text, navigation and layout are clear and easy to use, making it appropriate for all ages and abilities."
+  },
+  {
+    title: "Personal",
+    description: "Personalise the app to everyday life by adding personal words, phrases and photos to Lemmi's custom dictionary."
+  },
+  {
+    title: "Customisable",
+    description: "Select a voice, language, and colour scheme to suit the users needs. iOS users can add additional voices via the device's accessibility settings."
+  },
+  {
+    title: "Intuitive",
+    description: "Text prediction facilitates faster communication by creating quick access to the most commonly used words."
+  }
+];
+
+export const actions = {
+  contact: {
+    heading: "Have a question?",
+    link: "Get in touch"
+  },
+  waitlist: {
+    heading: "Join the Waitlist",
+    subheading: "Be the first to know when Lemmi is released.",
+    button: "Submit",
+    success: "Success! We'll email you again when Lemmi prepares to launch.",
+    error: "We were unable to sign you up. Please check your email and try again. If you continue to have difficulties, please get in touch."
+  },
+  social: {
+    heading: "Follow Us"
+  }
+};
+
+export const links = {
+  twitter: "https://twitter.com/lemmichat",
+  facebook: "https://www.facebook.com/lemmichat",
+  instagram: "https://www.instagram.com/lemmichat"
+}
